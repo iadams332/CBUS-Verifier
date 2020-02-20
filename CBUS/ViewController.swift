56cbf5ad175f10228dfb73ccd30e788c306921c2
Isaac Adams <isaac.adams332@gmail.com>
2020-02-20 13:35:34 -0500

    Initial Commit

---
+++ CBUS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CBUS
+//
+//  Created by Isaac Adams on 2/20/20.
+//  Copyright © 2020 UAHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
