012adc5542c2f7b2c160bfe9603c9c945bafc69e
Isaac Adams <isaac.adams332@gmail.com>
2020-02-20 18:51:48 -0500

    Designed the opening screen UI

--- CBUS/ViewController.swift
+++ CBUS/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundGradientView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Sets up the gradient background
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor(red: 0/255, green: 14/255, blue: 229/255, alpha: 1).cgColor, UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1).cgColor ]
+        gradientLayer.shouldRasterize = true
+        backgroundGradientView.layer.addSublayer(gradientLayer)
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 2)
+        
     }
-
-
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    //Makes the UIStatusBar text white
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 }
 
