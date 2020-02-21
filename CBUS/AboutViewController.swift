4512a2e59635a710b3786b8d127e77ef49b76e46
Isaac Adams <isaac.adams332@gmail.com>
2020-02-20 22:02:13 -0500

    Added in the about app page and set up segues

--- CBUS/AboutViewController.swift
+++ CBUS/AboutViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AboutViewController.swift
 //  CBUS
 //
 //  Created by Isaac Adams on 2/20/20.
@@ -8,25 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AboutViewController: UIViewController {
 
-    @IBOutlet var backgroundGradientView: UIView!
+    @IBOutlet var gradientBackgroundView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         //Sets up the gradient background
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [UIColor(red: 0/255, green: 14/255, blue: 229/255, alpha: 1).cgColor, UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1).cgColor ]
         gradientLayer.shouldRasterize = true
-        backgroundGradientView.layer.addSublayer(gradientLayer)
+        gradientBackgroundView.layer.addSublayer(gradientLayer)
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 2)
-        
     }
     
-    //Keeps the background from rotating
+    //Prevents the background from rotating
     override var shouldAutorotate: Bool {
         return false
     }
@@ -36,10 +35,19 @@ class ViewController: UIViewController {
         return .lightContent
     }
     
-    
-    @IBAction func aboutSegue(_ sender: Any) {
-        [self .performSegue(withIdentifier: "AboutViewController", sender: self)]
+    @IBAction func mainMenuSegue(_ sender: Any) {
+        [self .performSegue(withIdentifier: "mainMenuSegue", sender: self)]
     }
     
-}
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
